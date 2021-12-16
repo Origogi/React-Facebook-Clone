f3bf6321c0d0490a8f8820bbf71c81355f9471fb
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2021-12-16 17:08:36 +0900

    Add SidebarRow.js, SidebarRow.css

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./Sidebar.css"
+import SidebarRow from './SidebarRow'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
+            <SidebarRow />
             
         </div>
     )
