8e64318c5b9e1e01c7c6322eab905e9c705a592b
origogi <rlawjdxo1212@naver.com>
2021-12-17 23:37:48 +0900

    Complete StoryReel

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -1,14 +1,35 @@
 import React from "react";
+import Story from "./Story";
 import "./StoryReel.css";
 
 function StoryReel() {
   return (
     <div className="storyReel">
-      {/* Story */}
-      {/* Story */}
-      {/* Story */}
-      {/* Story */}
-      {/* Story */}
+      <Story
+        title="pyo apple"
+        image="http://image.tvdaily.co.kr/upimages/gisaimg/202011/1605750012_1566785.jpg"
+        profileSrc="https://image.fmkorea.com/files/attach/new2/20210327/494354581/3271844952/3481711232/99b983892094b5c6d2fc3736e15da7d1.jpg"
+      />
+      <Story
+        title="pyo apple"
+        image="http://image.tvdaily.co.kr/upimages/gisaimg/202011/1605750012_1566785.jpg"
+        profileSrc="https://image.fmkorea.com/files/attach/new2/20210327/494354581/3271844952/3481711232/99b983892094b5c6d2fc3736e15da7d1.jpg"
+      />
+      <Story
+        title="pyo apple"
+        image="http://image.tvdaily.co.kr/upimages/gisaimg/202011/1605750012_1566785.jpg"
+        profileSrc="https://image.fmkorea.com/files/attach/new2/20210327/494354581/3271844952/3481711232/99b983892094b5c6d2fc3736e15da7d1.jpg"
+      />
+      <Story
+        title="pyo apple"
+        image="http://image.tvdaily.co.kr/upimages/gisaimg/202011/1605750012_1566785.jpg"
+        profileSrc="https://image.fmkorea.com/files/attach/new2/20210327/494354581/3271844952/3481711232/99b983892094b5c6d2fc3736e15da7d1.jpg"
+      />
+      <Story
+        title="pyo apple"
+        image="http://image.tvdaily.co.kr/upimages/gisaimg/202011/1605750012_1566785.jpg"
+        profileSrc="https://image.fmkorea.com/files/attach/new2/20210327/494354581/3271844952/3481711232/99b983892094b5c6d2fc3736e15da7d1.jpg"
+      />
     </div>
   );
 }
