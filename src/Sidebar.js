f80d544b02b9e38cfbc6305d644fd7df01e6b2e1
origogi <rlawjdxo1212@naver.com>
2021-12-17 17:55:55 +0900

    add sidebarow

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,19 +1,28 @@
-import React from 'react'
-import "./Sidebar.css"
-import SidebarRow from './SidebarRow'
+import {
+  Chat,
+  ExpandMoreOutlined,
+  LocalHospital,
+  People,
+  StorefrontOutlined,
+  VideoLibrary,
+} from "@mui/icons-material";
+import { getDisplayName } from "@mui/utils";
+import React from "react";
+import "./Sidebar.css";
+import SidebarRow from "./SidebarRow";
 
 function Sidebar() {
-    return (
-        <div className='sidebar'>
-            <SidebarRow title="Pages"/>
-            <SidebarRow title="Friends"/>
-            <SidebarRow />
-            <SidebarRow />
-            <SidebarRow />
-            <SidebarRow />
-
-        </div>
-    )
+  return (
+    <div className="sidebar">
+      <SidebarRow src='' title='Origogi' />
+      <SidebarRow Icon={LocalHospital} title="COVID-19 Information Center" />
+      <SidebarRow Icon={People} title="Freinds" />
+      <SidebarRow Icon={Chat} title="Messenger" />
+      <SidebarRow Icon={StorefrontOutlined} title="Marketplace" />
+      <SidebarRow Icon={VideoLibrary} title="Videos" />
+      <SidebarRow Icon={ExpandMoreOutlined} title="Marketplace" />
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
