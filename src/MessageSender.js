e105f62cab738a67f124d28a9b476fcb84eb515f
origogi <rlawjdxo1212@naver.com>
2021-12-20 14:22:56 +0900

    Implement messageSender input

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -3,10 +3,26 @@ import React from "react";
 import "./MessageSender.css";
 
 function MessageSender() {
+
+   const handleSubmit = (e) => {
+     e.preventDefault();
+   }
+
   return (
     <div className="messageSender">
       <div className="messageSender__top">
         <Avatar />
+        <form>
+          <input
+            className="messageSender__input"
+            placeholder="What's on your mind"
+          />
+          <input placeholder="image URL (Optional)" />
+
+          <button onClick={handleSubmit} type="submit">
+            Hidden submit
+          </button>
+        </form>
       </div>
 
       <div className="messageSender__bottom"></div>
