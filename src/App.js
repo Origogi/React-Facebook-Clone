24e4e0d82cfffefbaa66c82b78588d0e40f40f30
origogi <rlawjdxo1212@naver.com>
2021-12-21 16:16:09 +0900

    Add Login.js, Login.css

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
+import Login from "./Login";
 
 function App() {
+  const user = "Origogi";
   return (
     <div className="app">
-      <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <div>
+          <Header />
 
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
