3cc4f754a03227f61954b5e833d9f79cff295433
origogi <rlawjdxo1212@naver.com>
2021-12-21 17:07:59 +0900

    fix image assets url

--- src/Feed.js
+++ src/Feed.js
@@ -10,18 +10,18 @@ function Feed() {
       <StoryReel />
       <MessageSender />
       <Post
-        profilePic="https://avatars.githubusercontent.com/u/35194820?v=4 "
+        profilePic="https://scontent-ssn1-1.xx.fbcdn.net/v/t1.18169-9/1010076_477783955633350_1900224762_n.png?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=lJOurCSMot4AX8k8KuN&tn=W3PhfU3NUdCI-4Su&_nc_ht=scontent-ssn1-1.xx&oh=00_AT98xLdFtXzHO--vKvdYc01TfjzaMCAQmu-9vS1Y9AIevw&oe=61E53CC1"
         message="Wow this works"
         timestamp="This is a timestamp"
         userName="Origogi"
-        image="https://blog.kakaocdn.net/dn/0q9tN/btqXJV8DnMJ/gtK996QYY7yrvvSxrVPAbK/img.gif"
+        image="https://scontent-ssn1-1.xx.fbcdn.net/v/t39.30808-6/238073759_4104163382995371_5769779480473560584_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=R922wpBHkekAX9M2v_t&tn=W3PhfU3NUdCI-4Su&_nc_ht=scontent-ssn1-1.xx&oh=00_AT9gVYxRK20dDiCfGU1FxO269Jk6PQ6IqXu1RpcYX8ivDA&oe=61C70AF1"
       />
       <Post
-        profilePic="https://avatars.githubusercontent.com/u/35194820?v=4 "
+        profilePic="https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/69563903_3014898355248679_7611766459034763264_n.png?_nc_cat=108&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=MrcA3Dbph2sAX83eyi3&tn=W3PhfU3NUdCI-4Su&_nc_ht=scontent-ssn1-1.xx&oh=00_AT-As7-ZoRE5mOcSqBszMYnbjD0TUBiiAPra8_lH-V1bmQ&oe=61E697D2"
         message="Wow this works"
         timestamp="This is a timestamp"
         userName="Origogi"
-        image="https://i2.tcafe2a.com/210611/014c7e45bc0d9edcc4d0c23e9f9876fc_1623423537_0078.gif"
+        image="https://1.bp.blogspot.com/-b1_n6tOHvWU/YKMssWEjo-I/AAAAAAAAQjk/vIJQsAPUpRQKxR44GoCbm3CtRgr8tVBKACLcBGAsYHQ/s0/Android_NewForDevelopers_1024x512_updated.png"
       />
     </div>
   );
--- src/StoryReel.js
+++ src/StoryReel.js
@@ -6,29 +6,29 @@ function StoryReel() {
   return (
     <div className="storyReel">
       <Story
-        title="pyo apple"
-        image="http://image.tvdaily.co.kr/upimages/gisaimg/202011/1605750012_1566785.jpg"
-        profileSrc="https://image.fmkorea.com/files/attach/new2/20210327/494354581/3271844952/3481711232/99b983892094b5c6d2fc3736e15da7d1.jpg"
+        title="Flutter"
+        image="https://assets.alexandria.raywenderlich.com/books/be0da83c17e547776b6ff910c5109b836d4b642faedff5a9a04d18d4375714e0/images/ad5295501b90ad84026582f5d9a8996c1cb9713176e6c390088fa8e67829b5c5/w594.png"
+        profileSrc="https://cdn.dribbble.com/users/17559/screenshots/6664357/figma.png?compress=1&resize=400x300"
       />
       <Story
-        title="pyo apple"
-        image="http://image.tvdaily.co.kr/upimages/gisaimg/202011/1605750012_1566785.jpg"
-        profileSrc="https://image.fmkorea.com/files/attach/new2/20210327/494354581/3271844952/3481711232/99b983892094b5c6d2fc3736e15da7d1.jpg"
+        title="React"
+        image="https://images.velog.io/images/kykim_dev/post/04ac0062-a506-427d-8370-8fe191a49a11/What-is-React-JS.jpeg"
+        profileSrc="https://scontent-ssn1-1.xx.fbcdn.net/v/t1.18169-9/1010076_477783955633350_1900224762_n.png?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=lJOurCSMot4AX8k8KuN&tn=W3PhfU3NUdCI-4Su&_nc_ht=scontent-ssn1-1.xx&oh=00_AT98xLdFtXzHO--vKvdYc01TfjzaMCAQmu-9vS1Y9AIevw&oe=61E53CC1"
       />
       <Story
-        title="pyo apple"
-        image="http://image.tvdaily.co.kr/upimages/gisaimg/202011/1605750012_1566785.jpg"
-        profileSrc="https://image.fmkorea.com/files/attach/new2/20210327/494354581/3271844952/3481711232/99b983892094b5c6d2fc3736e15da7d1.jpg"
+        title="Android"
+        image="https://images.kbench.com/kbench/article/2021_10/k225807p1n1-s.jpg"
+        profileSrc="https://images.otwojob.com/product/t/H/1/tH1496SipRLCtEL.jpg"
       />
       <Story
-        title="pyo apple"
-        image="http://image.tvdaily.co.kr/upimages/gisaimg/202011/1605750012_1566785.jpg"
-        profileSrc="https://image.fmkorea.com/files/attach/new2/20210327/494354581/3271844952/3481711232/99b983892094b5c6d2fc3736e15da7d1.jpg"
+        title="iOS"
+        image="https://i0.wp.com/iphoneislam.com/wp-content/uploads/2021/09/ios15-welcome.jpg?fit=1280%2C720&ssl=1"
+        profileSrc="https://www.cctvnews.co.kr/news/photo/202005/202710_202771_3715.png"
       />
       <Story
-        title="pyo apple"
-        image="http://image.tvdaily.co.kr/upimages/gisaimg/202011/1605750012_1566785.jpg"
-        profileSrc="https://image.fmkorea.com/files/attach/new2/20210327/494354581/3271844952/3481711232/99b983892094b5c6d2fc3736e15da7d1.jpg"
+        title="Github"
+        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZfuaKS6AtczM6MvnSJKjVoSRkC2YIl_vzFHlA8-3q7K7pneSIeqIZQEZS55WMxdbRO7I&usqp=CAU"
+        profileSrc="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
       />
     </div>
   );
